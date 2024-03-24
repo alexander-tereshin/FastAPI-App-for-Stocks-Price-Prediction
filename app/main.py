a8a19f53deeb44bf52dc803a5af5449f48cca0e7
Alexander <alexandr.tereshin@outlook.com>
2024-03-24 22:31:06 +0300

    Add Dockerfile

--- app/main.py
+++ app/main.py
@@ -1,17 +1,26 @@
-import sys
-import pathlib
-
 from fastapi import FastAPI
+from redis import asyncio as aioredis
+from contextlib import asynccontextmanager
+
 from app.parser.parser_router import router as parser_router
 from app.predictor.predict_router import router as predict_router
 
 
 app = FastAPI(
-    title="FastAPI App for Stocks Price Prediction"
+    title="FastAPI App for Stocks Price Prediction",
+    # lifespan=lifespan
 )
 
 app.include_router(parser_router)
 app.include_router(predict_router)
+
+@app.get("/", summary="Root", operation_id='root__get')
+async def root():
+    """
+    Root endpoint that returns a greeting message.
+    """
+    return {"message": "Hello, User!"}
+
 
 
 if __name__ == "__main__":
